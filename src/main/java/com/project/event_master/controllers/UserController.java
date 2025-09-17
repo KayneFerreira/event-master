830277152d664e9714e5fe329bf17c2458877c2e
KayneFerreira <kayne.ferreira@outlook.com>
2025-09-17 15:31:01 -0300

    Project updated: - Added all basic CRUD functionality to UserService layer; - Added all basic end points to UserController layer;

--- src/main/java/com/project/event_master/controllers/UserController.java
+++ src/main/java/com/project/event_master/controllers/UserController.java
@@ -1,9 +1,17 @@
 package com.project.event_master.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,10 +29,40 @@ public class UserController {
         this.service = service;
     }
 
-    // CRUD
+    /** Create new user */
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public UserEntity createNewUser(@RequestBody UserEntity user) {
         return service.createNewUser(user);
+    }
+
+    /** List all existing users  */
+    @GetMapping
+    @ResponseBody
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<UserEntity> findAllUsers() {
+        return service.findAllUsers();
+    }
+
+    /** Find user by id */
+    @GetMapping("/{id}")
+    @ResponseBody
+    @ResponseStatus(code = HttpStatus.OK)
+    public Optional<UserEntity> findUserById(@PathVariable Long id) {
+        return service.findUserById(id);
+    }
+
+    /** Update existing user */
+    @PutMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public UserEntity updateUser(@RequestBody UserEntity user, @PathVariable Long id) {
+        return service.updateUser(user, id);
+    }
+
+    /** Delete user by id */
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Long id) {
+        service.deleteUser(id);
     }
 }
--- src/main/java/com/project/event_master/services/UserService.java
+++ src/main/java/com/project/event_master/services/UserService.java
@@ -1,5 +1,8 @@
 package com.project.event_master.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.project.event_master.entities.UserEntity;
@@ -15,9 +18,35 @@ public class UserService {
         this.repository = repository;
     }
 
-    // CRUD
+    /** Create new user */
     public UserEntity createNewUser(UserEntity user) {
         return repository.save(user);
+    }
+
+    /** List all existing users  */
+    public List<UserEntity> findAllUsers() {
+        return repository.findAll();
+    }
+
+    // CHANGE -> MUST THROW EXCEPTION (CREATE CUSTOM EXCEPTION)
+    /** Find user by id */
+    public Optional<UserEntity> findUserById(Long id) {
+        return repository.findById(id);
+    }
+
+    // WARN: MAY NEED CHANGES
+    /** Update existing user */
+    public UserEntity updateUser(UserEntity user, Long id) {
+        UserEntity updateUser = repository.findById(id).get();
+        updateUser.setName(user.getName());
+        updateUser.setBirthDate(user.getBirthDate());
+        return repository.save(updateUser);
+    }
+
+    // CHANGE -> MUST THROW EXCEPTION (CREATE CUSTOM EXCEPTION)
+    /** Delete user by id */
+    public void deleteUser(Long id) {
+        repository.deleteById(id);
     }
 
 }
